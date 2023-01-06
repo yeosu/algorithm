ff8ef65b440b8cdd1d41595c216e87dc9f235e0d
eunhyeok <ceh2805@gmail.com>
2023-01-06 22:53:11 +0900

    #격자판 합
    격자판 행, 열, 대각선 합 중 가장 큰 수 구하기

---
+++ src/BingoSum.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BingoSum {
+
+    public int solution(int n, int[][] arr){
+        int answer = Integer.MIN_VALUE;
+        int sum1;//행의 합
+        int sum2;//열의 합
+        for (int i = 0; i < n; i++) {
+            sum1=sum2=0;
+            for (int j = 0; j < n; j++) {
+                sum1+=arr[i][j];
+                sum2+=arr[j][i];
+            }
+            answer=Math.max(answer, sum1);
+            answer=Math.max(answer, sum2);
+        }
+        sum1=sum2=0;
+        for (int i = 0; i < n; i++) {
+            sum1+=arr[i][i];
+            sum2+=arr[i][n-i-1];
+        }
+        answer=Math.max(answer, sum1);
+        answer=Math.max(answer, sum2);
+
+        return answer;
+    }
+    public static void main(String[] args){
+        BingoSum bingoSum = new BingoSum();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j]= in.nextInt();
+            }
+        }
+        System.out.print(bingoSum.solution(n, arr));
+    }
+}
