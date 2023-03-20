b232a4fa4af7b3bfed30fe8d2745f2c880557273
eunhyeok <ceh2805@gmail.com>
2023-03-20 21:31:42 +0900

    #알고리즘 문제 풀이
     - 최대 길이 연속 부분수열(복합적 풀이)

---
+++ src/MaxSubnet.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class MaxSubnet {
+    public int solution(int n, int k, int[] arr){
+        int answer =0;
+        int cnt=0;
+        int lt=0;
+
+        for (int rt = 0; rt < n; rt++) {
+            if(arr[rt]==0) cnt++;
+            while(cnt>k){
+                if(arr[lt]==0) cnt--;
+                lt++;
+            }
+            answer=Math.max(answer, rt-lt+1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        MaxSubnet T = new MaxSubnet();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print(T.solution(n,k,arr));
+    }
+}
