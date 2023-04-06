8e38eac300519cd0889d4274617595bcc98bfc10
eunhyeok <ceh2805@gmail.com>
2023-04-06 21:07:16 +0900

    #알고리즘 문제 풀이
     - 버블 정렬

---
+++ src/BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class BubbleSort {
+    public int[] solution(int n, int[] arr){
+        for(int i=0;i<n-1;i++){
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        BubbleSort T = new BubbleSort();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
+    }
+}
